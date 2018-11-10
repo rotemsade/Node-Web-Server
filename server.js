2c3cd9b7ae7d91e9b9f14c09cb30387b43ab7237
Rotem Sade <rotemsade@Rotems-MacBook-Pro.local>
2018-11-10 17:19:40 +0200

    Adding Projects page

--- server.js
+++ server.js
@@ -55,7 +55,14 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    portfolioMessage: 'Portfolio Page Here'
   });
 });
 
